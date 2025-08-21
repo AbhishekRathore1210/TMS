1ebb6024128e360179fa7caa632e94cd2ca9438d
Abhishek Rathore <abhishekrathore19229785@gmail.com>
2025-08-21 22:15:14 +0530

    hardcode mongo url

--- backend/dist/database/index.mjs
+++ backend/dist/database/index.mjs
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 console.log(process.env.MONGO_DATABASE_URL);
 export default async function connectToMongoDB() {
     console.log(process.env.MONGO_DATABASE_URL);
-    await mongoose.connect(`${process.env.MONGO_DATABASE_URL}`);
+    await mongoose.connect("mongodb+srv://abhishek19229785:43qmdZ4QoDPxDpct@tmscluster.oenvvf8.mongodb.net/test?retryWrites=true&w=majority&appName=TMSCLUSTER");
     console.log("Connect to MongoDB Database!");
 }
 //# sourceMappingURL=index.mjs.map
